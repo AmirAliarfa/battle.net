565b67d2883546377b9466100c42335f137364f4
unknown <you@example.com>
2023-10-22 09:33:38 +0330

    battle.net

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ './login.html' ],
+  theme: {
+    extend: {
+      colors: {
+        'bg-amirali':'#15171E'
+      }
+    },
+  },
+  plugins: [],
+}
+
